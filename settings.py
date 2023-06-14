a012b91ea14772de0c3f569905f60a7edfa017dc
fengzhou-shawn <66395156+fengzhou-shawn@users.noreply.github.com>
2023-06-14 15:53:39 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = " "
 
 # openAi 官方 api
 URL = "https://api.openai.com/v1/chat/completions"
